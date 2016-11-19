9ce5dd293f5f5b2a869e67c1b1f77b9ff7f1b529
Peter Miklos <pt.miklos@gmail.com>
2016-11-19 22:53:14 +0000

    request is connected to booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119224428) do
+ActiveRecord::Schema.define(version: 20161119225149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20161119224428) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "space_id"
+    t.integer  "request_id"
+    t.index ["request_id"], name: "index_bookings_on_request_id", using: :btree
     t.index ["space_id"], name: "index_bookings_on_space_id", using: :btree
   end
 
@@ -38,6 +40,7 @@ ActiveRecord::Schema.define(version: 20161119224428) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "bookings", "requests"
   add_foreign_key "bookings", "spaces"
   add_foreign_key "requests", "spaces"
 end
