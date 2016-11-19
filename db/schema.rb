c75a428f3ae3d9513ed25ef4494c3b95984aa0f4
Peter Miklos <pt.miklos@gmail.com>
2016-11-19 22:43:27 +0000

    request model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119223930) do
+ActiveRecord::Schema.define(version: 20161119224244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20161119223930) do
     t.datetime "updated_at", null: false
     t.integer  "space_id"
     t.index ["space_id"], name: "index_bookings_on_space_id", using: :btree
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "spaces", force: :cascade do |t|
