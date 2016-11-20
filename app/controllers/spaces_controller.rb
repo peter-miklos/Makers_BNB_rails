386e723dadd61d12eaa43fe29cb4bf5e600f166c
Peter Miklos <pt.miklos@gmail.com>
2016-11-20 16:51:34 +0000

    space controller updated, spaces/new.html.haml created

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,7 +1,13 @@
 class SpacesController < ApplicationController
   include SpacesHelper
 
+  before_action :authenticate_user!, :except => [:index, :show]
+
   def index
     @spaces = Space.all
+  end
+
+  def new
+    @space = Space.new
   end
 end
