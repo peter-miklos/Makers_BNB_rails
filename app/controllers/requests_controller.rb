25f7746395fda3ab899304fe9017567d3e049c8f
Peter Miklos <pt.miklos@gmail.com>
2016-11-23 18:59:55 +0000

    request controller updated, requests/new view created

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -8,10 +8,12 @@ class RequestsController < ApplicationController
 
   def new
     @space = Space.find(params[:space_id])
+    @owner = User.find(@space.user_id)
+    @request_date = session[:sds]
     request = Request.where(space_id: @space.id, user_id: current_user.id)
-    if (current_user && current_user.id != @space.user_id && !request)
+    if (current_user && current_user.id != @space.user_id && request.empty?)
       @request = Request.new
-    elsif (current_user && current_user.id != @space.user_id && request)
+    elsif (current_user && current_user.id != @space.user_id && !request.request?)
       redirect_to space_path(@space), alert: "You've already created a request"
     elsif (current_user && current_user.id == @space.user_id)
       redirect_to space_path(@space), alert: "You cannot create a request for your post"
