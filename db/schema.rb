d699c7f5658d61cf01da3ad4324c0d6c5f2e8919
Peter Miklos <pt.miklos@gmail.com>
2016-11-23 19:06:56 +0000

    request model updated with message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120134849) do
+ActiveRecord::Schema.define(version: 20161123190257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20161120134849) do
     t.datetime "updated_at", null: false
     t.integer  "space_id"
     t.integer  "user_id"
+    t.text     "message"
     t.index ["space_id"], name: "index_requests_on_space_id", using: :btree
     t.index ["user_id"], name: "index_requests_on_user_id", using: :btree
   end
